168be3e068df673bf5cad1604070d7e44418154b
NickBwalley <59914541+NickBwalley@users.noreply.github.com>
2020-09-27 19:43:59 +0300

    bbit

--- ATM_System_Cpp/main.cpp
+++ ATM_System_Cpp/main.cpp
@@ -1,10 +1,46 @@
 #include <iostream>
 
 using namespace std;
+//function prototypes
+void normalUser();
+void administrator();
 
 int main()
 {
-    cout << "Hello world!" << endl;
-    cout << "Hello World!..." << endl;
+    //variable definition
+    int option;
+    cout << "*****WELCOME TO BBIT2020 E-CASH SERVICES*****" << endl;
+    cout << "1. Normal User Login: " << endl;
+    cout << "2. Administrator Login: " << endl;
+    cout << "Choose an Option to proceed: " << endl;
+    cin >> option;
+    switch(option)
+    {
+    case 1:
+        normalUser();
+        break;
+    case 2:
+        administrator();
+        break;
+    default:
+        cout << "Invalid Input TRY AGAIN with 1 or 2 " << endl;
+    }
     return 0;
+}
+
+void normalUser()
+{
+    cout << "----------------------------------------------" << endl;
+    cout << "Welcome to Normal User Login Page: " << endl;
+    cout << "Enter Username: " << endl;
+    cout << "Enter Password: " << endl;
+    cout << "----------------------------------------------" << endl;
+}
+void administrator()
+{
+    cout << "----------------------------------------------" << endl;
+    cout << "Welcome to Registration and Deposit Page: " << endl;
+    cout << "Enter Administrator Username: " << endl;
+    cout << "Enter Administrator Password: " << endl;
+    cout << "----------------------------------------------" << endl;
 }
